d591140c410b6bcb3b628157ff7a36ce18be1ae3
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-09-07 19:36:18 +0700

    set layout of home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container class="home" fluid>
+    <v-row class="mx-1">
+      <v-col cols="2"><div class="red">sadgh</div></v-col>
+      <v-col cols="8" class="pt-0">
+        <v-row>
+          <v-col cols="12"><div class="green">FSADGF</div></v-col>
+          <v-col cols="12"><div class="green">FSADGF</div></v-col>
+        </v-row>
+      </v-col>
+      <v-col cols="2"><div class="blue">FSADG</div></v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
 };
 </script>
+
+<style scoped>
+div {
+  min-height: 44vh;
+}
+.red {
+  background-color: red;
+  height: 100%;
+}
+.green {
+  background-color: green;
+  height: 100%;
+}
+.blue {
+  background-color: blue;
+  height: 100%;
+}
+</style>
