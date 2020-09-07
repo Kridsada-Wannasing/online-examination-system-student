3821d21180feb8b853791dd57880292e7b802bb2
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-09-07 21:42:51 +0700

    create navigation drawer component

---
+++ src/mixins/navigationDrawerLists.js
@@ -0,0 +1,12 @@
+export const navigationDrawerLists = {
+  data() {
+    return {
+      items: [
+        { title: "หน้าหลัก", icon: "mdi-home" },
+        { title: "แจ้งผลการสอบ", icon: "mdi-file-document" },
+        { title: "การแจ้งเตือน", icon: "mdi-bell-outline" },
+        { title: "ห้องสอบ", icon: "mdi-pencil-box-outline" },
+      ],
+    };
+  },
+};
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container class="home" fluid>
     <v-row class="mx-1">
-      <v-col cols="2"><div class="red">sadgh</div></v-col>
+      <v-col cols="2"><NavigationDrawer /></v-col>
       <v-col cols="8" class="pt-0">
         <v-row>
           <v-col cols="12"><div class="green">FSADGF</div></v-col>
@@ -14,12 +14,21 @@
 </template>
 
 <script>
+import NavigationDrawer from "@/components/NavigationDrawer.vue";
+
 export default {
   name: "Home",
+  components: {
+    NavigationDrawer,
+  },
 };
 </script>
 
 <style scoped>
+.home {
+  background: transparent linear-gradient(227deg, #1a73e8 0%, #6ec448 100%) 0%
+    0% no-repeat padding-box;
+}
 div {
   min-height: 44vh;
 }
