b702562ff205a2113d4acb760b2849a5001339aa
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-09-08 20:25:17 +0700

    add enter to examination page

---
+++ src/views/EnterToExamination.vue
@@ -0,0 +1,25 @@
+<template> </template>
+
+<script>
+export default {
+  data() {
+    return {
+      password: "",
+      examinationId: 0,
+    };
+  },
+  methods: {
+    enterToExamination() {
+      this.$store
+        .dispatch("examination/enterToExamination", {
+          password: this.password,
+          examinationId: this.examinationId,
+        })
+        .then((response) => this.$router.push({ name: "Examination" }))
+        .catch((err) => alert(err));
+    },
+  },
+};
+</script>
+
+<style></style>
