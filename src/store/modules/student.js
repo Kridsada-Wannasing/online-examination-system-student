77517444ed855b75a366e646a5f7928816203ebe
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-09-09 20:05:38 +0700

    config axios

--- src/store/modules/student.js
+++ src/store/modules/student.js
@@ -1,3 +1,4 @@
+import studentService from "../../api/services/student";
 import axios from "axios";
 
 export const namespaced = true;
@@ -24,23 +25,23 @@ export const mutations = {
 
 export const actions = {
   async login({ commit }, credentials) {
-    const res = await axios.post(
-      "http://localhost:8000/student/login",
-      credentials
-    );
-    await commit("SET_STUDENT", res.data);
-    return res;
+    const res = await studentService.login(credentials);
+    commit("SET_STUDENT", res.data);
   },
   logout({ commit }) {
     commit("CLEAR_STUDENT");
   },
   async updateMe({ commit }, credentials) {
-    const data = await axios.patch(
-      "http://localhost:8000/student/updateMe",
-      credentials
-    );
-    commit("SET_STUDENT", data);
-    return data;
+    const updatedMe = await studentService.updateMe(credentials);
+    commit("SET_STUDENT", updatedMe);
+  },
+  async updatePassword(newPassword) {
+    const updatedPassword = await studentService.updatePassword(newPassword);
+    return updatedPassword.data.status;
+  },
+  async forgotPassword(email) {
+    const message = await studentService.forgotPassword(email);
+    return message.data.status;
   },
 };
 
